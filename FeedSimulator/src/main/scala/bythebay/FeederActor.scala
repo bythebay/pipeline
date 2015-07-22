66ec2f25496f24bf7c34f6a84cac2f0150f6e664
Ryan Knight <ryan@grandcloud.com>
2015-07-22 07:21:18 -0700

    cleaning up code for feeder

--- FeedSimulator/src/main/scala/bythebay/FeederActor.scala
+++ FeedSimulator/src/main/scala/bythebay/FeederActor.scala
@@ -1,9 +1,6 @@
 package bythebay
 
 import akka.actor.{Props, Actor, ActorLogging}
-import bythebay.FeederActor.Initialize
-import bythebay.FeederActor.SendNextLine
-import bythebay.FeederActor.ShutDown
 import kafka.producer.KeyedMessage
 
 import scala.concurrent.duration.Duration
@@ -13,7 +10,9 @@ import scala.concurrent.duration._
  * This keeps the file handle open and just reads on line at fixed time ticks.
  * Not the most efficient implementation, but it is the easiest.
  */
-class FeederActor extends Actor with ActorLogging with FeederExtensionActor{
+class FeederActor extends Actor with ActorLogging with FeederExtensionActor {
+
+  import bythebay.FeederActor.SendNextLine
 
   var counter = 0
 
@@ -21,32 +20,24 @@ class FeederActor extends Actor with ActorLogging with FeederExtensionActor{
 
   val feederTick = context.system.scheduler.schedule(Duration.Zero, 100.millis, self, SendNextLine)
 
-  var ratingsDataSet:Iterator[String] = Iterator.empty
+  var ratingsDataSet:Iterator[String] = initRatingsFile()
 
   def receive = {
-  	case Initialize =>
-	    println("In FeederActor - starting file feeder")
-      initRatingsFile
-
-    case SendNextLine =>
-      if (ratingsDataSet.isEmpty)
-        initRatingsFile
-
+    case SendNextLine if ratingsDataSet.hasNext =>
       val nxtRating = ratingsDataSet.next()
-      println(nxtRating)
+      log.info(s"Sending next rating: $nxtRating")
       feederExtension.producer.send(new KeyedMessage[String, String](feederExtension.kafkaTopic, nxtRating))
 
   }
 
-  def initRatingsFile() {
+  def initRatingsFile() = {
     val source = scala.io.Source.fromFile(feederExtension.dataDirectory + "/ratings.dat")
-    ratingsDataSet = source.getLines
+    source.getLines()
   }
 }
 
 object FeederActor {
 	val props = Props[FeederActor]
-	case object Initialize
 	case object ShutDown
 	case object SendNextLine
 
