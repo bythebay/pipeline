b60182a325f43e22c5d84ccca9e7d030c3267c51
Evan Chan <velvia@gmail.com>
2015-08-07 17:14:08 -0700

    Refactor streaming and feeder into separate projects; build assembly for feeder

--- bythebay-feed.sh
+++ bythebay-feed.sh
@@ -1,3 +1,8 @@
-echo ...Starting Feeder...
+#!/bin/bash
+#
+echo ...Building Feeder app...
 cd $PIPELINE_HOME
-nohup sbt run &
+sbt feeder/assembly
+echo ...Starting Feeder app...
+java -Xmx1g -jar feeder/target/scala-2.10/feeder-assembly-1.0.jar 2>&1 1>feeder-out.log &
+echo    logs available by tailing feeder-out.log
