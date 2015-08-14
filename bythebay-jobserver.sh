3fe7603b8c6e3eed4c7b80b0374c01facfd9d385
Evan Chan <velvia@gmail.com>
2015-08-14 08:32:39 -0700

    Add jobserver start script, upgrade cass spark connector to 1.4.0-M3

---
+++ bythebay-jobserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo ...Starting Spark JobServer...
+~/$SPARK_JOBSERVER_HOME/server_start.sh --packages org.apache.spark:spark-streaming-kafka-assembly_2.10:1.4.1,com.datastax.spark:spark-cassandra-connector-java_2.10:1.4.0-M3
+
+echo ...Waiting 10 seconds to upload jars...
+sleep 10
+
+curl --data-binary @$SPARK_JOBSERVER_HOME/job-server-tests/target/scala-2.10/job-server-tests_2.10-0.5.2.jar localhost:8099/jars/test
+curl --data-binary @$PIPELINE_HOME/streaming/target/scala-2.10/streaming_2.10-1.0.jar localhost:8099/jars/streaming
